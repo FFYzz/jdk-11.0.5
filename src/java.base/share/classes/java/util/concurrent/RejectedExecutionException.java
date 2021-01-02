f00254890bfe15f89fea97b38668dccdc4ee1fa9
FFYzz <cryptochen95@gmail.com>
2021-01-02 11:17:33 +0800

    RejectedExecutionException 补充注释

--- src/java.base/share/classes/java/util/concurrent/RejectedExecutionException.java
+++ src/java.base/share/classes/java/util/concurrent/RejectedExecutionException.java
@@ -42,6 +42,11 @@ package java.util.concurrent;
  * @since 1.5
  * @author Doug Lea
  */
+
+/**
+ * 拒绝执行异常
+ * 继承自 RuntimeException
+ */
 public class RejectedExecutionException extends RuntimeException {
     private static final long serialVersionUID = -375805702767069545L;
 
